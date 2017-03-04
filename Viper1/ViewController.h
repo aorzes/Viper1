89c52c16279beb4f5d965a80b0e65ef664c2893a
Anton Orzes <antonorzes@Antons-Mac-mini.local>
2017-03-04 20:27:52 +0100

    Initial Commit

---
+++ Viper1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Viper1
+//
+//  Created by Anton Orzes on 04/03/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Viper1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Viper1
+//
+//  Created by Anton Orzes on 04/03/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
